606701308fe254896e77dc1c6470f6d848a0f84c
TheSilhouette <thesilhouette1705@tutanota.com>
2022-07-03 09:09:14 +0800

    styled most of the page
    - also added paper colour theme for code highlight
    - extracted posts loop into its own component

--- _config.ts
+++ _config.ts
@@ -1,5 +1,6 @@
 import lume from "lume/mod.ts";
 import code_highlight from "lume/plugins/code_highlight.ts";
+import date from "lume/plugins/date.ts";
 import imagick from "lume/plugins/imagick.ts";
 import sass from "lume/plugins/sass.ts";
 import search from "lume/plugins/search.ts";
@@ -17,5 +18,12 @@ site.use(code_highlight());
 site.use(imagick());
 site.use(sass());
 site.use(search());
+site.use(
+  date({
+    formats: {
+      BRIT_DATE: "dd/MM/yyyy",
+    },
+  })
+);
 
 export default site;
